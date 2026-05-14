0923354996bb32ad9db7bab81300112b83d1f5eb
Darknessfreenetsquad <sibongakonketha@gmail.com>
2026-05-14 21:12:08 +0200

    Create setting.js

---
+++ system/setting.js
@@ -0,0 +1,25 @@
+global.prefix = [
+  ".", "🥹","🥶","🔥"
+];
+global.publik = true
+global.linkOwner = "https://t.me/darknessfreenetsquad"
+global.idsaluran = "120363388565127379@newsletter"
+global.linkch = "https://whatsapp.com/channel/0029VbCISBm3LdQXfcLaao3Q"
+global.owner = ["27634988678"] 
+global.nameowner = "𝙍𝙀𝘿 𝘿𝙍𝘼𝙂𝙊𝙉 𝘿𝙁𝙎" 
+global.author = ["27824275911"] 
+global.namaauthor = "𝙍𝙀𝘿 𝘿𝙍𝘼𝙂𝙊𝙉 𝘿𝙁𝙎" 
+global.namebot = "𝙍𝙀𝘿-𝙓𝙈𝘿" 
+global.bot = {
+  newsletterJid: 
+    "120363388565127379@newsletter",
+};
+
+//======================
+global.mess = { 
+author: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀɴ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜsᴇʀ* ✘', 
+owner: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴛʜᴇ ᴏᴡɴᴇʀ* ✘',
+premium: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀ* ✘',
+succes: '*sᴜᴄᴄᴇs ☑*︎'
+}
+//======================
